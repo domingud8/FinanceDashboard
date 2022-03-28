e7a3ba7530a10cee6ac51c1337ddf862da918d0d
Daniela Dominguez Damiani <90041931+domingud8@users.noreply.github.com>
2022-03-28 14:35:42 +0200

    Update setup.sql

--- server/setup.sql
+++ server/setup.sql
@@ -1,1 +1,34 @@
 
+DROP TABLE IF EXISTS income;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS account;
+
+CREATE TABLE account(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR NOT NULL,
+    current_balance NUMERIC(10,5) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    categories ARRAY 
+);
+
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    account_id INTEGER NOT NULL REFERENCES account (id),
+    payee  VARCHAR ,
+    date   DATE NOT NULL,
+    amount NUMERIC NOT NULL,
+    group VARCHAR,
+    category VARCHAR,
+    description VARCHAR  
+);
+
+CREATE TABLE income (
+    id SERIAL PRIMARY KEY,
+    account_id INTEGER NOT NULL REFERENCES account (id),
+    date_trans   DATE NOT NULL,
+    amount NUMERIC(10,2) NOT NULL,
+    category  VARCHAR
+);
